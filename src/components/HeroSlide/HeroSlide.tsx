260e1c92e2b61603d2e72b5bdf3b0e39e2080b92
TanLocIT200600 <77155886+TanLocIT200600@users.noreply.github.com>
2022-01-26 08:32:39 +0700

    1

--- src/components/HeroSlide/HeroSlide.tsx
+++ src/components/HeroSlide/HeroSlide.tsx
@@ -1,8 +1,11 @@
 import axios from 'axios';
 import React, { useEffect, useRef, useState } from 'react';
-import SwiperCore, { Autoplay } from "swiper";
 import './heroSlide.scss';
 import Slider from "react-slick"
+import apiConfig from '../../api/apiConfig';
+import { useNavigate } from 'react-router-dom';
+import SwiperCore, { Autoplay } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 
 interface IMyData {
@@ -29,7 +32,6 @@ interface IMyData {
 }
 
 const HeroSlide = () => {
-
   SwiperCore.use([Autoplay]);
 
   const [movie, setMovie] = useState<IMyData[]>();
@@ -39,8 +41,8 @@ const HeroSlide = () => {
       const URL: string = "https://api.themoviedb.org/3/movie/popular?api_key=761dea999bb72d9517bae0bb585b4df0"
       try {
         const res = await axios.get(URL);
-        console.log(res.data.results.slice(1, 4));
-        setMovie(res.data.results.slice(1, 4));
+        console.log(res.data.results.slice(1, 2));
+        setMovie(res.data.results.slice(1, 2));
       }
       catch (err) {
         console.log(err);
@@ -49,28 +51,80 @@ const HeroSlide = () => {
     fetchMovieList()
   }, [])
 
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true
-  };
-
-
   return (
     <div className="hero-slide">
-      <Slider {...settings}>
+      <Swiper
+        modules={[Autoplay]}
+        grabCursor={true}
+        spaceBetween={0}
+        slidesPerView={1}
+      // autoplay={{ delay: 3000 }}
+      >
         {
-          movie?.map((item) => (
-            <img key={item.id} src={`https://image.tmdb.org/t/p/w500/${item.backdrop_path}`} alt='' width="1000" height="800" />
+          movie?.map((item, i) => (
+            <SwiperSlide key={i}>
+              {({ isActive }) => (
+                <HeroSlideItem item={item} className={`${isActive ? 'active' : ''}`} />
+              )}
+            </SwiperSlide>
           ))
         }
-      </Slider>
+      </Swiper>
+      {
+        // movie?.map((item, i) => <TrailerModal key={i} item={item} />)
+      }
     </div>
   );
 };
+
+const HeroSlideItem = (props: any) => {
+
+  let navigate = useNavigate();
+
+  const item = props.item;
+
+  const background = apiConfig.originalImage(item.backdrop_path ? item.poster_path : item.backdrop_path);
+
+  const setModalActive = async () => {
+    const modal: Element | null = document.querySelector(`#modal_${item.id}`);
+
+    // const videos = await tmdbApi.getVideos(category.movie, item.id);
+
+    // if (videos.results.length > 0) {
+    //   const videSrc = 'https://www.youtube.com/embed/' + videos.results[0].key;
+    //   modal.querySelector('.modal__content > iframe').setAttribute('src', videSrc);
+    // } else {
+    //   modal.querySelector('.modal__content').innerHTML = 'No trailer';
+    // }
+
+    modal?.classList.toggle('active');
+  }
+
+  return (
+    <div
+      className={`hero-slide__item ${props.className}`}
+      style={{ backgroundImage: `url(${background})` }}
+    >
+      <div className="hero-slide__item__content container">
+        <div className="hero-slide__item__content__info">
+          <h2 className="title">{item.title}</h2>
+          <div className="overview">{item.overview}</div>
+          <div className="btns">
+            <button className="watch-now" onClick={() => navigate('/movie/' + item.id)}>
+              Watch now
+            </button>
+            <button className="watch-trailer" onClick={setModalActive}>
+              Watch trailer
+            </button>
+          </div>
+        </div>
+        <div className="hero-slide__item__content__poster">
+          <img src={apiConfig.w500Image(item.poster_path)} alt="" />
+        </div>
+      </div>
+    </div>
+  )
+}
 
 
 export default HeroSlide;
